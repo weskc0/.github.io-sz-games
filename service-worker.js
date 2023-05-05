ca1e15b191418c0ad2c4c86f029e8cf016fade90
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-05-04 21:53:38 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,13 +9,13 @@ workbox.routing.registerRoute(
 
 const cacheName = 'cache-v1';
 // List the files to precache
-const precacheResources = ['/', '.index,html', '/games/game.html'];
+const precacheResources = ['/', '/index.html', '/games/game.html'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
   console.log('Service worker install event!');
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
-  
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return fetch('/henry-airship/')
@@ -40,15 +40,12 @@ self.addEventListener('activate', (event) => {
 });
 
 
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    }),
-  );
-});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function(error) {
+        console.error('Fetch error:', error);
+        return caches.match('/index.html');
+      })
+    );
+  });
