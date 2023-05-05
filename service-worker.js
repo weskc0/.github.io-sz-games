b2fc12c13b64aada3a1cc9438487c929c7751144
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-05-04 22:11:13 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,30 +9,38 @@ workbox.routing.registerRoute(
 
 const cacheName = 'Sz-Games';
 // List the files to precache
-const precacheResources = ['/', '/index.html', '/games/game.html'];
+const precacheResources = ['/',
+ '/index.html',
+ '/games/game.html',
+ '/games/tunnel-rush/',
+ '/games/tunnel-rush/index.html',
+
+ '/games/tunnel-rush/style/fullscreen.png',
+ '/games/tunnel-rush/style/progressEmpty.Dark.png',
+ '/games/tunnel-rush/style/progressEmpty.Light.png',
+ '/games/tunnel-rush/style/progressFull.Dark.png',
+ '/games/tunnel-rush/style/progressFull.Light.png',
+ '/games/tunnel-rush/style/progressLogo.Dark.png',
+ '/games/tunnel-rush/style/progressLogo.Light.png',
+ '/games/tunnel-rush/style/style.css',
+ '/games/tunnel-rush/style/webgl-logo.png',
+ '/games/tunnel-rush/UnityLoader.js',
+ '/games/tunnel-rush/UnityProgeass.js',
+ '/games/tunnel-rush/build.json',
+ '/games/tunnel-rush/nohack_tunnel_rush_v9wasmframework.unityweb',
+ '/games/tunnel-rush/tunnel_rush_v9code.unityweb',
+ '/games/tunnel-rush/tunnel_rush_v9data.unityweb',
+ '/games/tunnel-rush/tunnel_rush_v9framework.unityweb',
+ '/games/tunnel-rush/tunnel_rush_v9memory.unityweb',
+ '/games/tunnel-rush/tunnel_rush_v9wasmcode.unityweb',
+ '/games/tunnel-rush/tunnel_rush_v9wasmframework.unityweb',
+];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
   console.log('Service worker install event!');
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return fetch('/henry-airship/')
-        .then(response => response.text())
-        .then(html => {
-          const doc = new DOMParser().parseFromString(html, 'text/html');
-          const files = Array.from(doc.querySelectorAll('a'))
-            .map(link => link.getAttribute('href'))
-            .filter(href => /\.js$|\.css$|\.png$|\.jpg$|\.gif$/.test(href));
-
-          return Promise.all(files.map(file => {
-            return fetch('/henry-airship/' + file)
-              .then(response => cache.put(response.url, response))
-          }));
-        });
-    })
-  );
 });
 
 self.addEventListener('activate', (event) => {
