6e399d752d5eead88b288fe146b000cee6abd3dc
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-05-04 21:47:59 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,49 @@ workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.NetworkFirst()
 );
+
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = ['/', '.index,html', '/games/game.html'];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return fetch('/henry-airship/')
+        .then(response => response.text())
+        .then(html => {
+          const doc = new DOMParser().parseFromString(html, 'text/html');
+          const files = Array.from(doc.querySelectorAll('a'))
+            .map(link => link.getAttribute('href'))
+            .filter(href => /\.js$|\.css$|\.png$|\.jpg$|\.gif$/.test(href));
+
+          return Promise.all(files.map(file => {
+            return fetch('/henry-airship/' + file)
+              .then(response => cache.put(response.url, response))
+          }));
+        });
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
