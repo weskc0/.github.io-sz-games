5a78d6e029c5c066f1ec83b4d31be21e09b24345
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-06-19 21:07:41 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -11,29 +11,6 @@ const cacheName = 'Sz-Games';
 // List the files to precache
 const precacheResources = ['/',
  '/index.html',
- '/games/game.html',
- '/games/tunnel-rush/',
- '/games/tunnel-rush/index.html',
-
- '/games/tunnel-rush/style/fullscreen.png',
- '/games/tunnel-rush/style/progressEmpty.Dark.png',
- '/games/tunnel-rush/style/progressEmpty.Light.png',
- '/games/tunnel-rush/style/progressFull.Dark.png',
- '/games/tunnel-rush/style/progressFull.Light.png',
- '/games/tunnel-rush/style/progressLogo.Dark.png',
- '/games/tunnel-rush/style/progressLogo.Light.png',
- '/games/tunnel-rush/style/style.css',
- '/games/tunnel-rush/style/webgl-logo.png',
- '/games/tunnel-rush/UnityLoader.js',
- '/games/tunnel-rush/UnityProgeass.js',
- '/games/tunnel-rush/build.json',
- '/games/tunnel-rush/nohack_tunnel_rush_v9wasmframework.unityweb',
- '/games/tunnel-rush/tunnel_rush_v9code.unityweb',
- '/games/tunnel-rush/tunnel_rush_v9data.unityweb',
- '/games/tunnel-rush/tunnel_rush_v9framework.unityweb',
- '/games/tunnel-rush/tunnel_rush_v9memory.unityweb',
- '/games/tunnel-rush/tunnel_rush_v9wasmcode.unityweb',
- '/games/tunnel-rush/tunnel_rush_v9wasmframework.unityweb',
 ];
 
 // When the service worker is installing, open the cache and add the precache resources to it
