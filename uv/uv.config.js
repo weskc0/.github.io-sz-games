8c10b46f558366885384f57f4ba17842b7fcfae6
sz-games <116248710+sz-games@users.noreply.github.com>
2024-10-18 18:17:21 -0700

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,14 @@
+
+self.__uv$config = {
+    prefix: '/uv/service/',
+
+    bare: 'https://solitary-haze-aa1f.projectorsum.workers.dev/',
+    
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
